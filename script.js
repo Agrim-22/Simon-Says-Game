bc2a06d173b81467a5523a2d1eb918b8b4c3402f
Agrim Tiwari <agrimtiwari833@gmail.com>
2024-12-24 22:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let level=0;
 let h2=document.querySelector('h2');
 
 
-document.addEventListener("keypress",function(event){
+document.addEventListener("keypress,click",function(event){
     if(started==false){
         console.log("game started");
         started=true;
